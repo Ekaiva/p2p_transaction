958d0ae031738be09846ccef634f6e79a1b12f78
Ankit Yadav <ankit.yadav@paymendia.in>
2023-05-11 10:28:33 +0530

    SetType Page

--- src/Layout.js
+++ src/Layout.js
@@ -5,6 +5,7 @@ import AdList from "./Components/AdList";
 import Home from "./Components/Home";
 import LoginPage from "./Components/LoginPage";
 import SignUp from "./Components/SignUp";
+import SetType from "./Components/SetType";
 
 const Layout = () => {
   return (
@@ -15,6 +16,7 @@ const Layout = () => {
           <Route path="/sign-up" element={<SignUp/>}/>
           <Route path="/ads" element={<AdList/>}/>
           <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/set-type" element={<SetType/>}/>
           <Route path="/404" element={<PageNotFound/>}/>
          <Route path="*" element={<Navigate to="/404" replace />} />
     </Routes>
